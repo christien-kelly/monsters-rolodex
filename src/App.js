6808259f3b31f1eaa9569cd66d732e6cd223b650
christien-kelly <christien@ridewithshare.com>
2021-03-12 10:54:11 -0400

    adding files for project pages

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
           handleChange ={this.handleChange}
